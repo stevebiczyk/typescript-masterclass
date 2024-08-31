4cb89ab5da1bd1564b5fe452133ab555fe5e0cc3
stevebiczyk <stevebiczyk@gmail.com>
2024-08-31 13:52:22 +0100

    my first typescript program

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 "use strict";
-console.log("Hello world!");
+function addNumbers(a, b) {
+    return a + b;
+}
+console.log(addNumbers(2, 3));
+//console.log(addNumbers("text", 3));
--- index.ts
+++ index.ts
@@ -1,1 +1,5 @@
-console.log("Hello world!");
+function addNumbers(a: number, b: number) {
+  return a + b;
+}
+console.log(addNumbers(2, 3));
+//console.log(addNumbers("text", 3));
