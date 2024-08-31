d7b69d75c3455208e71975016b4cbb0b1db83aeb
stevebiczyk <stevebiczyk@gmail.com>
2024-09-01 00:15:31 +0100

    primitive types: string, number, boolean

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 "use strict";
-function addNumbers(a, b) {
-    return a + b;
-}
-console.log(addNumbers(2, 3));
-//console.log(addNumbers("text", 3));
+//String types
+var firstname = "John";
+firstname = 1;
+let car = "BMW";
+const city = "New York";
+let students = 32;
+let studentAsString = students.toString();
+// Number types
+var age = 32;
+let weight = 100;
+const numberOfItems = 99;
+numberOfItems = 100;
+let stringToNumber = parseInt("111");
+// Boolean types
+let isStudent = true;
+const alwaysStudent = true;
+let minimumAge = age > 18 ? true : false;
--- index.ts
+++ index.ts
@@ -1,5 +1,20 @@
-function addNumbers(a: number, b: number) {
-  return a + b;
-}
-console.log(addNumbers(2, 3));
-//console.log(addNumbers("text", 3));
+//String types
+
+var firstname: string = "John";
+firstname = 1;
+let car: string = "BMW";
+const city: string = "New York";
+let students = 32;
+let studentAsString = students.toString();
+
+// Number types
+var age: number = 32;
+let weight: number = 100;
+const numberOfItems = 99;
+numberOfItems = 100;
+let stringToNumber: number = parseInt("111");
+
+// Boolean types
+let isStudent: boolean = true;
+const alwaysStudent = true;
+let minimumAge: boolean = age > 18 ? true : false;
