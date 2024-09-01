0f37e5b93bdf80a9995d42222aa63c50d14ccf9b
stevebiczyk <stevebiczyk@gmail.com>
2024-09-02 00:16:05 +0100

    any type

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 "use strict";
-/**
- * Assign the correct types to each of the following variables
- */
-let message = "Hello, TypeScript!";
-let age = 42;
-let isStudent = true;
-let fetched = null;
-let user = undefined;
-let largeNumber = 9007199254740991n;
-let unique = Symbol("uniqueSymbol");
+let firstname = "John";
+firstname = 123;
+firstname = [];
+return param;
