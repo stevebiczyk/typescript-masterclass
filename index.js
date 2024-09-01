6a0a3cc57279e20fa4e65169f5c024781d4040c8
stevebiczyk <stevebiczyk@gmail.com>
2024-09-01 10:41:01 +0100

    bigInt type

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 "use strict";
-let user;
-console.log(user); // undefined
-let userRole;
-userRole = null;
-console.log(userRole); // null
-console.log(user === userRole); // false
+const safeInt = Number.MAX_SAFE_INTEGER;
+console.log(safeInt);
+const safeIntPlusOne = safeInt + 1;
+console.log(safeIntPlusOne);
+const safeIntPlusTwo = safeInt + 2;
+console.log(safeIntPlusTwo);
+let bigInt1 = BigInt(1234);
+let bigInt2 = BigInt(123456789n);
+console.log(bigInt1);
+let c = bigInt1 - bigInt2;
+console.log(c);
+let d = 12345.6789n;
+let e = Math.log(bigInt1);
--- index.ts
+++ index.ts
@@ -1,8 +1,19 @@
-let user: undefined;
-console.log(user); // undefined
+const safeInt = Number.MAX_SAFE_INTEGER;
+console.log(safeInt);
 
-let userRole: null;
-userRole = null;
-console.log(userRole); // null
+const safeIntPlusOne = safeInt + 1;
+console.log(safeIntPlusOne);
 
-console.log(user === userRole); // false
+const safeIntPlusTwo = safeInt + 2;
+console.log(safeIntPlusTwo);
+
+let bigInt1: bigint = BigInt(1234);
+let bigInt2: bigint = BigInt(123456789n);
+console.log(bigInt1);
+
+let c: bigint = bigInt1 - bigInt2;
+console.log(c);
+
+let d: bigint = 12345.6789n;
+
+let e: bigint = Math.log(bigInt1);
