9b38878bdac28a6bb2d5698e0164f20e88dd4fb7
stevebiczyk <stevebiczyk@gmail.com>
2024-11-01 17:47:17 +0000

    inference, annotation, duck typing and declaration

--- index.ts
+++ index.ts
@@ -14,8 +14,14 @@ type CustomString = string;
 type CustomNumber = number;
 type CustomDate = Date;
 type CustomSymbol = Symbol;
-
+//annotation
 let firstName: CustomString = "Istvan";
 let age: CustomNumber = 46;
 let birthday: CustomDate = new Date();
 let unique: CustomSymbol = Symbol("symbol");
+
+function addNumbers(a: number, b: number) {
+  return a + b;
+}
+//inference
+let finalResult = addNumbers(10, 15);
