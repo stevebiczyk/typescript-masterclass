33d821a922ad53fdde5d8806e752b0875270ef7c
stevebiczyk <stevebiczyk@gmail.com>
2024-11-02 16:07:37 +0000

    understanding type hierarchy

--- index.ts
+++ index.ts
@@ -1,6 +1,14 @@
-type CustomDate = Date;
-type CustomString = string;
+type check = any extends unknown ? true : false; // true
+type check2 = string extends any ? true : false; // true
+type check3 = null extends any ? true : false; // true
+type check4 = undefined extends void ? true : false; // true
+type check5 = [] extends Object ? true : false; // true
+type check6 = Function extends Object ? true : false; // true
 
-type TrueString = CustomString extends string ? true : false;
-type ConditionalNumber = CustomDate extends Date ? number : string;
-type DateAssignment = CustomDate extends Date ? Date : undefined;
+//a function declaration that never completes and returns a never type
+const throwError = (errMessage: string) => {
+  throw new Error(errMessage);
+};
+
+let strings: Object = ["a", "b", "c"];
+let myFunction: Function = () => 2;
