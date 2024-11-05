9b2ad567f873ffd05d72ba2aec5805c11a3b1645
stevebiczyk <stevebiczyk@gmail.com>
2024-11-05 22:29:51 +0000

    type casting

--- index.ts
+++ index.ts
@@ -1,14 +1,19 @@
-type check = any extends unknown ? true : false; // true
-type check2 = string extends any ? true : false; // true
-type check3 = null extends any ? true : false; // true
-type check4 = undefined extends void ? true : false; // true
-type check5 = [] extends Object ? true : false; // true
-type check6 = Function extends Object ? true : false; // true
+let firstName = <any>"Istvan";
+let lastName = "Biczyk" as any;
 
-//a function declaration that never completes and returns a never type
-const throwError = (errMessage: string) => {
-  throw new Error(errMessage);
+// User fromm API
+let user = {
+  name: "Mark",
+  email: "mark@email.com",
 };
 
-let strings: Object = ["a", "b", "c"];
-let myFunction: Function = () => 2;
+type User {
+    name: string;
+    email: string;
+}
+
+function fetchUser() {
+  return user as User;
+}
+
+const userFromAPI = fetchUser();
