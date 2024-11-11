cdc154d83919582e88602aeaf0d52a9487a9331f
stevebiczyk <stevebiczyk@gmail.com>
2024-11-11 23:46:31 +0000

    nested Objects

--- index.ts
+++ index.ts
@@ -8,20 +8,26 @@
 //   date: new Date(),
 // };
 
+type Author = {
+  name: string;
+  age: number;
+  email: string;
+};
+
 type Post = {
   title: string;
   content: string;
   date: Date;
+  author: Author;
 };
 
 let post: Post = {
   title: "This is the blog title",
   content: "This is the blog content",
   date: new Date(),
-};
-
-let post1: Post = {
-  title: "This is the blog title",
-  content: "This is the blog content",
-  date: new Date(),
+  author: {
+    name: "Mike",
+    age: 30,
+    email: "mike@mike.com",
+  },
 };
