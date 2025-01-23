b88dc70c2b857aafcf48be056585777cab5a6f12
stevebiczyk <stevebiczyk@gmail.com>
2025-01-23 14:22:00 +0000

    tuples

--- index.ts
+++ index.ts
@@ -1,39 +1,15 @@
-let a: number[] = [1, 2, 3, 4, 5];
-let b: Array<string> = ["a", "b", "c", "d", "e"];
-let c: (string | number | boolean)[] = ["a", 1, "b", 2, "c", 3, true, false];
+let person: [string, string, number] = ["John", "Doe", 30];
 
-type Pilot = {
-  name: string;
-  licenseNumber: string;
-  age: number;
-};
+type User = [string, string, number, string?];
+let user: User = ["John", "Doe", 30, "user@email.com"];
 
-type Seats = {
-  [keyof: string]: string;
-};
+type listOfStudents = [number, boolean, ...string[]];
 
-type Airplane = {
-  model: string;
-  flightNumber: string;
-  timeOfDeparture: Date;
-  timeOfArrival: Date;
-  pilot: Pilot;
-  seats: Seats;
-};
+const mathStudents: listOfStudents = [1, true, "John", "Doe", "Jane", "Doe"];
 
-let airplane: Airplane = {
-  model: "Boeing 747",
-  flightNumber: "JH-123",
-  timeOfDeparture: new Date(),
-  timeOfArrival: new Date(),
-  pilot: {
-    name: "John Doe",
-    licenseNumber: "1234",
-    age: 45,
-  },
-  seats: {
-    "1A": "John Doe",
-    "1B": "Jane Doe",
-    "1C": "Sam Doe",
-  },
-};
+type StringBooleansNumber = [string, ...boolean[], number];
+type BooleansStringNumber = [...boolean[], string, number];
+
+let stringBooleansNumber: StringBooleansNumber = ["John", true, false, 30];
+
+let booleansStringNumber: BooleansStringNumber = [true, false, "John", 30];
