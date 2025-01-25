5d0753481088844141519975ab437fca6b87ecad
stevebiczyk <stevebiczyk@gmail.com>
2025-01-25 20:04:54 +0000

    enums

--- index.ts
+++ index.ts
@@ -1,13 +1,42 @@
-let number: readonly number[] = [1, 2, 3];
+const STATUS_LOADING = "loading";
+const STATUS_LOADED = "loaded";
 
-number.push(4); // Error: Property 'push' does not exist on type 'readonly number[]'.
+STATUS_LOADING = "loaded"; // Error: Cannot assign to 'STATUS_LOADING' because it is a constant.
 
-type ReadOnlyTuple = readonly [string, string, number];
+enum Direction {
+  Up, // 0
+  Down, // 1
+  Left, // 2
+  Right, // 3
+}
 
-let person: ReadOnlyTuple = ["John", "Doe", 30];
-person[0] = "Jane"; // Error: Index signature in type 'readonly ["John", "Doe", 30]' only permits reading.
+console.log(Direction.Up); // 0
+Direction.Left = 10; // Error: Cannot assign to 'Left' because it is a read-only property.
 
-type a = Readonly<(string | number)[]>;
-type b = ReadonlyArray<string | number>;
+enum Direction2 {
+  Up = "UP",
+  Down = "DOWN",
+  Left = "LEFT",
+  Right = "RIGHT",
+}
 
-type c = Readonly<[number, string, number]>;
+console.log(Direction2.Right); // RIGHT
+
+enum Roles {
+  ADMIN = "admin",
+  AUTHOR = "author",
+  EDITOR = "editor",
+}
+type Person = {
+  name: string;
+  email: string;
+  password: string;
+  role: Roles;
+};
+
+const Person1: Person = {
+  name: "John",
+  email: "john@email.com",
+  password: "pwd123",
+  role: Roles.ADMIN,
+};
