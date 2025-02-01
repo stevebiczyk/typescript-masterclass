0f0a5f3ca5c83903ea608a7b4b79fe8446eb2d0a
stevebiczyk <stevebiczyk@gmail.com>
2025-02-01 23:27:51 +0000

    computed enums, enums as Unions and Types

--- index.ts
+++ index.ts
@@ -1,24 +1,43 @@
-enum Direction {
-  Up, // 0
-  Down, // 1
-  Left, // 2
-  Right, // 3
+enum AccessPermissions {
+  None = 0,
+  Read = 1,
+  Write = 2,
+  ReadWrite = Read + Write,
+  Delete = 4,
+  All = ReadWrite | Delete,
 }
 
-const enum EDirection {
-  Up, // 0
-  Down, // 1
-  Left, // 2
-  Right, // 3
+console.log(AccessPermissions.ReadWrite); // 3
+console.log(AccessPermissions.All); // 7
+
+enum ShapeKind {
+  Circle = "circle",
+  Square = "square",
 }
+// Circle = "circle" | Square = "square"
 
-const ODirection = {
-  Up: 0,
-  Down: 1,
-  Left: 2,
-  Right: 3,
-} as const;
+type Circle = {
+  kind: ShapeKind.Circle;
+  radius: number;
+};
 
-let eDirection = EDirection.Up;
-let direction = Direction.Right;
-ODirection.Right = "newvalue"; // Error: Cannot assign to 'Right' because it is a read-only property.
+type Square = {
+  kind: ShapeKind.Square;
+  sideLength: number;
+};
+
+let circle: Circle = {
+  kind: ShapeKind.Circle,
+  radius: 100,
+};
+
+function printShape(shape: ShapeKind) {
+  switch (shape.kind) {
+    case ShapeKind.Circle:
+      console.log("Circle", shape.radius);
+      break;
+    case ShapeKind.Square:
+      console.log("Square", shape.sideLength);
+      break;
+  }
+}
