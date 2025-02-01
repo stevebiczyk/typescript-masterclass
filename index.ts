a41b62fa03917d08b23d1f6738ce9926c2cc87c9
stevebiczyk <stevebiczyk@gmail.com>
2025-02-01 14:22:37 +0000

    Enums vs Objects

--- index.ts
+++ index.ts
@@ -1,8 +1,3 @@
-const STATUS_LOADING = "loading";
-const STATUS_LOADED = "loaded";
-
-STATUS_LOADING = "loaded"; // Error: Cannot assign to 'STATUS_LOADING' because it is a constant.
-
 enum Direction {
   Up, // 0
   Down, // 1
@@ -10,33 +5,20 @@ enum Direction {
   Right, // 3
 }
 
-console.log(Direction.Up); // 0
-Direction.Left = 10; // Error: Cannot assign to 'Left' because it is a read-only property.
-
-enum Direction2 {
-  Up = "UP",
-  Down = "DOWN",
-  Left = "LEFT",
-  Right = "RIGHT",
+const enum EDirection {
+  Up, // 0
+  Down, // 1
+  Left, // 2
+  Right, // 3
 }
 
-console.log(Direction2.Right); // RIGHT
+const ODirection = {
+  Up: 0,
+  Down: 1,
+  Left: 2,
+  Right: 3,
+} as const;
 
-enum Roles {
-  ADMIN = "admin",
-  AUTHOR = "author",
-  EDITOR = "editor",
-}
-type Person = {
-  name: string;
-  email: string;
-  password: string;
-  role: Roles;
-};
-
-const Person1: Person = {
-  name: "John",
-  email: "john@email.com",
-  password: "pwd123",
-  role: Roles.ADMIN,
-};
+let eDirection = EDirection.Up;
+let direction = Direction.Right;
+ODirection.Right = "newvalue"; // Error: Cannot assign to 'Right' because it is a read-only property.
