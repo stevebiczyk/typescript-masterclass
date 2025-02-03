d9f3584a095798886c9e4581b71f168acfb34d8a
stevebiczyk <stevebiczyk@gmail.com>
2025-02-03 13:51:25 +0000

    Arrays and Enums practice

--- index.ts
+++ index.ts
@@ -1,43 +1,30 @@
-enum AccessPermissions {
-  None = 0,
-  Read = 1,
-  Write = 2,
-  ReadWrite = Read + Write,
-  Delete = 4,
-  All = ReadWrite | Delete,
+// 1. Create an array of numbers that only accepts numbers.
+
+let numberArray: number[] = [1, 2, 3, 4, 5];
+
+// 2. Create an array of strings that only accepts strings.
+
+let stringArray: string[] = ["Hello", "World"];
+// 3. Create the tiple Person that only accepts a string (name) and a number (age).
+
+let person: [string, number] = ["John", 30];
+
+// 4. Create the readonly array Colors that only accepts strings, and the readonly tuple point that holds two numbers (x, y).
+
+const colors: readonly string[] = ["Red", "Blue", "Green"];
+const point: readonly [number, number] = [10, 20];
+
+// 5. Create an Enum called StatusEnum that has the following values: Active, Inactive, Pending.
+enum StatusEnum {
+  Active = "active",
+  Inactive = "inactive",
+  Pending = "pending",
 }
 
-console.log(AccessPermissions.ReadWrite); // 3
-console.log(AccessPermissions.All); // 7
+// 6. Create an object as const called Status that has the same structure as StatusEnum.
 
-enum ShapeKind {
-  Circle = "circle",
-  Square = "square",
-}
-// Circle = "circle" | Square = "square"
-
-type Circle = {
-  kind: ShapeKind.Circle;
-  radius: number;
-};
-
-type Square = {
-  kind: ShapeKind.Square;
-  sideLength: number;
-};
-
-let circle: Circle = {
-  kind: ShapeKind.Circle,
-  radius: 100,
-};
-
-function printShape(shape: ShapeKind) {
-  switch (shape.kind) {
-    case ShapeKind.Circle:
-      console.log("Circle", shape.radius);
-      break;
-    case ShapeKind.Square:
-      console.log("Square", shape.sideLength);
-      break;
-  }
-}
+const Status = {
+  Active: "active",
+  Inactive: "inactive",
+  Pending: "pending",
+} as const;
