3557dea396bf405faa3488b838749b9a04ab388b
stevebiczyk <stevebiczyk@gmail.com>
2025-02-03 15:42:06 +0000

    declaring functions

--- index.ts
+++ index.ts
@@ -1,30 +1,19 @@
-// 1. Create an array of numbers that only accepts numbers.
-
-let numberArray: number[] = [1, 2, 3, 4, 5];
-
-// 2. Create an array of strings that only accepts strings.
-
-let stringArray: string[] = ["Hello", "World"];
-// 3. Create the tiple Person that only accepts a string (name) and a number (age).
-
-let person: [string, number] = ["John", 30];
-
-// 4. Create the readonly array Colors that only accepts strings, and the readonly tuple point that holds two numbers (x, y).
-
-const colors: readonly string[] = ["Red", "Blue", "Green"];
-const point: readonly [number, number] = [10, 20];
-
-// 5. Create an Enum called StatusEnum that has the following values: Active, Inactive, Pending.
-enum StatusEnum {
-  Active = "active",
-  Inactive = "inactive",
-  Pending = "pending",
+// named function
+function intro(name: string, age: number): string {
+  return `Hello, my name is ${name} and I am ${age} years old.`;
 }
 
-// 6. Create an object as const called Status that has the same structure as StatusEnum.
+//function expression
+const intro2 = function (name: string, age: number): string {
+  return `Hello, my name is ${name} and I am ${age} years old.`;
+};
 
-const Status = {
-  Active: "active",
-  Inactive: "inactive",
-  Pending: "pending",
-} as const;
+// arrow function
+const intro3 = (name: string, age: number): string => {
+  return `Hello, my name is ${name} and I am ${age} years old.`;
+};
+
+// const intro4 = (name: string, age: number): number => {
+//   return `Hello, my name is ${name} and I am ${age} years old.`;
+// };
+//* Error: Type 'string' is not assignable to type 'number'.
